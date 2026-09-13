d972a156c781b03f7c965f25f1623c92a6905f57
Rajveer kumar <kumarrajveer0155@gmail.com>
2026-09-13 22:49:43 +0530

    Fix templates and URL routing

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,49 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post//', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_create, name='post_create'),
-    path('post//delete/', views.post_delete, name='post_delete'),
-    path('register/', views.user_register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+
+    path(
+        '',
+        views.post_list,
+        name='post_list'
+    ),
+
+    path(
+        'post/<int:pk>/',
+        views.post_detail,
+        name='post_detail'
+    ),
+
+    path(
+        'post/new/',
+        views.post_create,
+        name='post_create'
+    ),
+
+    path(
+        'post/<int:pk>/delete/',
+        views.post_delete,
+        name='post_delete'
+    ),
+
+    path(
+        'register/',
+        views.user_register,
+        name='register'
+    ),
+
+    path(
+        'login/',
+        views.user_login,
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        views.user_logout,
+        name='logout'
+    ),
+
 ]
